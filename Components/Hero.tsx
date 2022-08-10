1bd1bbdf93c086aa7e076ce856f310d8ceefb968
Simon Börner <boernersimon@googlemail.com>
2022-08-10 16:33:53 +0200

    add favicon

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -5,52 +5,54 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div
-      id="home"
-      className="bg-gradient-to-b from-palette1/30 to-palette2/30 relative min-h-screen flex flex-col items-center justify-center xs:pt-20 snap-start select-none"
-    >
-      <motion.div
-        className="w-[200px] h-[200px]"
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.2,
-            },
-          },
-        }}
+    <>
+      <div
+        id="home"
+        className="bg-gradient-to-b from-palette1/30 to-palette2/30 relative min-h-screen flex flex-col items-center justify-center xs:pt-20 snap-start select-none"
       >
-        <Image
-          src={headshot}
-          layout="intrinsic"
-          height={250}
-          width={250}
-          className="rounded-full select-none drop-shadow-md p-10"
-          alt="a picture of my face"
-        ></Image>
-      </motion.div>
+        <motion.div
+          className="w-[200px] h-[200px]"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.2,
+              },
+            },
+          }}
+        >
+          <Image
+            src={headshot}
+            layout="intrinsic"
+            height={250}
+            width={250}
+            className="rounded-full select-none drop-shadow-md p-10"
+            alt="a picture of my face"
+          ></Image>
+        </motion.div>
 
-      <div className="py-10 text-center w-5/6 ">
-        <h1 className="text-4xl pb-8 font-sans">
-          Hi, I&apos;m <span className="text-accentblue">Simon</span>.
-        </h1>
-        <p className="text-xl">
-          A Leipzig-based Junior Frontend Developer. <br />I am currently
-          looking for a job. Feel free to contact me!
-        </p>
-      </div>
+        <div className="py-10 text-center w-5/6 ">
+          <h1 className="text-4xl pb-8 font-sans">
+            Hi, I&apos;m <span className="text-accentblue">Simon</span>.
+          </h1>
+          <p className="text-xl">
+            A Leipzig-based Junior Frontend Developer. <br />I am currently
+            looking for a job. Feel free to contact me!
+          </p>
+        </div>
 
-      <div>
-        <HiOutlineChevronDoubleDown className="h-16 w-8 animate-bounce mx-auto left-0 right-0 bottom-5 drop-shadow-md" />
+        <div>
+          <HiOutlineChevronDoubleDown className="h-16 w-8 animate-bounce mx-auto left-0 right-0 bottom-5 drop-shadow-md" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../Components/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>Simon Börner Frontend-Developer</title>
+        <meta
+          name="description"
+          content="Hire a Junior Frontend-Developer from Leipzig"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
