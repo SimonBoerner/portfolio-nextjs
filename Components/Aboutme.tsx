beaed08443c4a6f8151bcef874ee50ff005652a8
Simon Börner <boernersimon@googlemail.com>
2022-08-12 18:45:52 +0200

    gfn2

--- Components/Aboutme.tsx
+++ Components/Aboutme.tsx
@@ -41,7 +41,7 @@ const Aboutme = () => {
 
       <p className="w-5/6 md:w-4/6">
         This is just the beginning of my coding journey. There is so much more
-        to learn and I'm really curious about it! <br />
+        to learn and I'm really excited to learn more about it! <br />
       </p>
     </div>
   );
