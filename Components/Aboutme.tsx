9f610be26029ca6b11e84dc6f60a886f5ee710f2
Simon Börner <boernersimon@googlemail.com>
2022-08-12 18:39:35 +0200

    gfn

--- Components/Aboutme.tsx
+++ Components/Aboutme.tsx
@@ -4,19 +4,19 @@ const Aboutme = () => {
   return (
     <div
       id="aboutme"
-      className="relative flex flex-col items-center justify-center pt-40 min-h-screen bg-palette2/50 text-xl snap-start text-black pb-8"
+      className="relative flex flex-col items-center justify-center pt-40 min-h-screen bg-palette2/20 text-xl snap-start text-black pb-8"
     >
       <h1 className="absolute top-20 md:mx-auto text-3xl tracking-wide">
         [About Me]
       </h1>
       <p className="w-5/6 md:w-4/6">
-        I’ve always been enthusiastic by computers and doing creative things
-        with them. Most of my time I was using music software (Ableton Live),
-        because I love making music and sounddesign. Two years ago, my father
-        asked me if i could build him a homepage. After a fun journey through
-        the internet in which I found out how to build websites with Wordpress,
-        I decided that I want to level up my skills by learning how to create
-        websites and apps the "proper" way.
+        I’ve always been fascinated by computers and doing creative things with
+        them. Most of my time I was using music software (Ableton Live), because
+        I love making music and sounddesign. Two years ago, my father asked me
+        rather randomly if i could build him a homepage. After a fun journey
+        through the internet in which I found out how to build websites with
+        Wordpress, I decided that I want to level up my skills by learning how
+        to create websites and apps from scratch.
       </p>
       <br />
       <br />
@@ -41,10 +41,8 @@ const Aboutme = () => {
 
       <p className="w-5/6 md:w-4/6">
         This is just the beginning of my coding journey. There is so much more
-        to learn! <br />
+        to learn and I'm really curious about it! <br />
       </p>
-
-      <p className="w-5/6 md:w-4/6"></p>
     </div>
   );
 };
