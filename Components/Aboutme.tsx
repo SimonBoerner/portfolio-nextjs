663109d71682bdbf5a0dcb6143a33dc6cb482c28
Simon Börner <boernersimon@googlemail.com>
2023-03-07 21:03:57 +0100

    new text

--- Components/Aboutme.tsx
+++ Components/Aboutme.tsx
@@ -34,12 +34,15 @@ const Aboutme = () => {
         &nbsp;It was the perfect kickstart. There I learned the fundamental
         technologies and concepts of modern webdesign and got a good idea how to
         use them. The technologies and tools we were learning are listed on the
-        page below.
+        page below. Don't get me wrong. I'm not an expert of there technologies,
+        but I am willing to become one!
       </p>
 
       <p className="w-5/6 md:w-4/6 pb-4">
-        This is just the beginning of my coding journey. There is so much more
-        to learn and I'm really excited to learn more about it! <br />
+        I'm aware, that this is just the beginning of my coding journey. It
+        would be nice to get to know you and maybe continue that journey in your
+        company.
+        <br />
       </p>
     </div>
   );
