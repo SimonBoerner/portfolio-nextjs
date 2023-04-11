9cc194fdd35b1f677068ba5e4ff7e4dc1cf49f90
Simon Börner <boernersimon@googlemail.com>
2023-04-11 15:28:06 +0200

    update

--- Components/Skillcard.tsx
+++ Components/Skillcard.tsx
@@ -7,14 +7,8 @@ type SkillcardProps = {
 };
 
 const Skillcard = ({ icon, name, description }: SkillcardProps) => {
-  const [showHoverText, setShowHoverText] = useState(false);
-
   return (
-    <div
-      onMouseEnter={() => setShowHoverText(true)}
-      onMouseLeave={() => setShowHoverText(false)}
-      className="flex flex-col items-center justify-center w-[30%] drop-shadow-lg bg-accentblue/60 text-white rounded-md p-4 hover:bg-opacity-90 hover:scale-105 transition-transform "
-    >
+    <div className="flex flex-col items-center justify-center w-[30%] drop-shadow-lg bg-accentblue/60 text-white rounded-md p-4 hover:bg-opacity-90 hover:scale-105 transition-transform ">
       <>
         <div className="py-3">{icon}</div>
         <div className="pb-3">{name}</div>
